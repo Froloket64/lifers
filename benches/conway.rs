8d66d556929434a34a7ce97ccb3a6a378cf1c489
Froloket64 <ket.frol2006@gmail.com>
2024-05-26 18:52:51 +0300

    Use regular `bool` instead of `Cell` in benches

--- benches/conway.rs
+++ benches/conway.rs
@@ -1,21 +1,6 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use lifers::{engine::AutomatonBuilder, prelude::*};
 
-#[derive(Default)]
-struct Cell {
-    is_alive: bool,
-}
-
-impl RenderCell<String> for Cell {
-    fn render_cell(&self) -> String {
-        match self.is_alive {
-            true => "██",
-            false => "··",
-        }
-        .into()
-    }
-}
-
 pub fn conway_bench(c: &mut Criterion) {
     let mut game = prepare_game(Automaton::build((10, 10)));
 
@@ -30,17 +15,13 @@ pub fn conway_bench(c: &mut Criterion) {
     c.bench_function("conway [1000x1000]", |b| b.iter(|| game2.step()));
 }
 
-fn prepare_game(builder: AutomatonBuilder<Cell, usize>) -> Automaton<Cell, usize> {
+fn prepare_game(builder: AutomatonBuilder) -> Automaton<bool, usize> {
     builder
-        .init(|_| Cell { is_alive: false })
-        .map(|(x, y), _, cells| count_neighbors(cells, (x, y), 1, |c| c.is_alive))
-        .run(|(_, _), is_alive, neighbors_n| match is_alive.is_alive {
-            true => Cell {
-                is_alive: (2..=3).contains(neighbors_n),
-            },
-            false => Cell {
-                is_alive: *neighbors_n == 3,
-            },
+        .init(|_| false)
+        .map(|(x, y), _, cells| count_neighbors(cells, (x, y), 1, |is_alive| *is_alive))
+        .run(|(_, _), is_alive, neighbors_n| match is_alive {
+            true => (2..=3).contains(neighbors_n),
+            false => *neighbors_n == 3,
         })
 }
 
